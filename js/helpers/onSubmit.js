3458fa5795630c6d4d92e0d8146e3ff69cde07e8
Taliha <103150201+talihaq@users.noreply.github.com>
2022-11-14 12:30:15 +0000

    Compare lines
    Added logic to check if origin and destination stations are on the same line.

--- js/helpers/onSubmit.js
+++ js/helpers/onSubmit.js
@@ -8,26 +8,34 @@ function onSubmit () {
     const origin = document.getElementById("origin-list");
     const destination = document.getElementById("destination-list");
 
-    // Create empty variables for originSelection and destinationSelection.
-    let originSelection, destinationSelection;
+    // Create empty variables for origin/destination names, ids and lines.
+    let  originName, destinationName, originId, destinationId, originLine, destinationLine;
 
     // This function runs when the form is submitted via the submit button.
     searchForm.addEventListener("submit", (e) => {
         // Prevent default form submit behaviour.
         e.preventDefault();
 
-        // On submit, get the naptanId from the user's origin selection.
-        originSelection = origin.options[origin.selectedIndex].getAttribute("data-naptan-id");
-        // On submit, get the naptanId from the user's destination selection.
-        destinationSelection = destination.options[destination.selectedIndex].getAttribute("data-naptan-id");
+        // On submit, get origin and destination names, ids and lines.
+        originName = origin.options[origin.selectedIndex].value;
+        originId = origin.options[origin.selectedIndex].getAttribute("data-naptan-id");
+        originLine = origin.options[origin.selectedIndex].getAttribute("data-line-id");
 
-        // Use console.log to check if the origin and destination id's have come through on submit.
-        console.log("Origin Station:", originSelection);
-        console.log("Destination Station:", destinationSelection);
+        destinationName = destination.options[destination.selectedIndex].value;
+        destinationId = destination.options[destination.selectedIndex].getAttribute("data-naptan-id");
+        destinationLine = destination.options[destination.selectedIndex].getAttribute("data-line-id");
 
-        // Compare if destination and origin ids share same lineIds. 
-        // If yes, then the destination can be reached directly on the origin station's line.
-        // If no, then the user cannot reach their destination directly.
+        // Console.log to check if data is coming through correctly.
+        // console.log(`Origin is ${originName}, ${originId}. It is on the ${originLine} line. Destination is ${destinationName}, ${destinationId}. It is on the ${destinationLine} line`);
+
+        // Compare if destination and origin ids share same lineIds.
+        if (originLine === destinationLine) {
+            // If the origin and destination are on the same line, then the destination can be reached directly from that line.
+            console.log(`You can travel from ${originName} to ${destinationName} directly via the ${originLine} line.`);
+        } else {
+            // Otherwise the destination cannot be reached directly.
+            console.log(`There is no direct route from ${originName} to ${destinationName}.`);
+        };
     });
 };
 
